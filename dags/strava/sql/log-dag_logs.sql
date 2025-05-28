bc3f5e4134a8a3d847134d1e78f9356af331fb78
csabimvp <kellercsabii@gmail.com>
2025-05-28 21:00:52 +0100

    Cleaned up log files.

---
+++ dags/strava/sql/log-dag_logs.sql
@@ -0,0 +1,4 @@
+INSERT INTO log.dag_logs (data_items, description, finished, project_name, start, status_code, task_id, task_name) VALUES
+(0, 'OK', '2025-05-28 20:56:11', 'auth-strava', '2025-05-28 20:56:11', 200, '7XIpGK0WNu1GVTg0UsKaLcIKfoPfut', 'RequestRefreshToken'),
+(9, 'OK', '2025-05-28 20:56:12', 'strava', '2025-05-28 20:56:11', 200, '8rK8zeg8ylYeHXTG0IjLk9AnVpVPgA', 'FetchAthleteStats'),
+(787, 'OK', '2025-05-28 20:56:37', 'strava', '2025-05-28 20:56:12', 200, 'c4hrF1cSxtgy6FKInBBOQdQk8bGXJ6', 'FetchAllAthleteActivities');
